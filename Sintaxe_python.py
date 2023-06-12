082cb1c4bd4309c641962c39131c33562a0d3087
gustavosantos <gustavolsantos2022@gmail.com>
2023-06-11 23:50:11 -0300

    Py02

---
+++ Sintaxe_python.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+
+# A caixa tem que ter pelo menos um espaço ou mais.
+if 5 > 2:
+    print("Isso aí, cinco é maior que dois!")
+
+
+# A caixa não tem espaço, a sintexe está errada.
+if 5 > 2:
+print("Isso aí, cinco é maior quye dois!")    
+
+
+# a caixa pode não seguir um padrão de identação que o comando não for o mesmo.
+if 5 > 2:
+    print("Isso aí, cinco é maior que dois!")
+if 5 > 2:
+         print("Isso aí, cinco é maior 2 ")
+
+
+# Agora que o comando estiver na mesma condição precisa está com a mesma identação. 
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+ 
+ # variáveis
+X = 10
+Y = "Gustavo Lima dos Santos"
+
+print(X)    
+print(Y)
